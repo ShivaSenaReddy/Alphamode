960f9e80bbd2740c854ad492d78096a7d213dacb
JAGAPATHI Shiva Sena Reddy <shiva.sena.reddy.jagapathi@hexagon.com>
2024-08-12 13:19:02 +0530

    new lost battle

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
 var countDownDate = new Date("jan 1, 2025 18:40:25").getTime();
-var countDownDate1 = new Date("aug 11,2024 13:00:25").getTime();
+var countDownDate1 = new Date("aug 12,2024 13:00:25").getTime();
 console.log(countDownDate1);
 // Update the count down every 1 second
 var x = setInterval(function () {
