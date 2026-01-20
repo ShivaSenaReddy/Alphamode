4db8dead93430dcec85a0e394cc1899965d9316c
JAGAPATHI Shiva Sena Reddy <shiva.sena.reddy.jagapathi@hexagon.com>
2026-01-20 09:28:26 +0530

    day1 easier 19th jan

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
 var countDownDate = new Date("jun 10, 2026 18:00:25").getTime();
-var countDownDate1 = new Date("mar 30,2026 18:00:25").getTime();
+var countDownDate1 = new Date("jan 19,2026 18:00:25").getTime();
 let nthDay = document.getElementById("nth-day");
 console.log(countDownDate1);
 // Update the count down every 1 second
