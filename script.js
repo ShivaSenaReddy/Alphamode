10dd8fe0e06bf07a4fec66e97d0afb775f76d643
JAGAPATHI Shiva Sena Reddy <shiva.sena.reddy.jagapathi@octave.com>
2026-08-20 16:39:47 +0530

    aug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Sep 10, 2027 18:00:25").getTime();
-var countDownDate1 = new Date("Aug 8,2026 18:00:25").getTime();
+var countDownDate = new Date("Sep 22, 2026 18:00:25").getTime();
+var countDownDate1 = new Date("Aug 21,2026 18:00:25").getTime();
 let nthDay = document.getElementById("nth-day");
 console.log(countDownDate1);
 // Update the count down every 1 second
